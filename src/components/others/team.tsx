56b87c6e0dd215045fd495f48c479fe976be74a4
jeanflores99 <ajiliof@gmail.com>
2022-10-20 15:20:00 -0500

    wraper icon del team agregado a cada integrante

--- src/components/others/team.tsx
+++ src/components/others/team.tsx
@@ -1,4 +1,10 @@
 import { TittleCustom } from './tittleCustom'
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaPinterest,
+} from 'react-icons/fa'
 import NextImage from 'next/image'
 export const Team = () => {
   return (
@@ -10,13 +16,21 @@ export const Team = () => {
             <NextImage
               src="/images/person1.jpg"
               layout="fill"
-              // width={350}
-              // height={350}
-              className="absolute w-full h-full rounded-lg"
+              className="absolute w-full h-full rounded-lg z-0"
             />
+            <div className="absolute bottom-9 w-full z-40">
+              <div className="flex flex-row gap-3 mx-auto w-[120px] h-[40px]  justify-center items-center rounded bg-[#AD8E6D] text-white">
+                <FaFacebookF />
+                <FaTwitter />
+                <FaLinkedinIn />
+                <FaPinterest />
+              </div>
+            </div>
           </div>
-          <div className='flex flex-col items-center justify-center gap-1 mt-3'>
-            <h4 className='text-white text-[20px] font-semibold text-center '>Fynley Wilkinson</h4>
+          <div className="flex flex-col items-center justify-center gap-1 mt-3">
+            <h4 className="text-white text-[20px] font-semibold text-center ">
+              Fynley Wilkinson
+            </h4>
             <h6>Managing Partner</h6>
           </div>
         </div>
@@ -29,9 +43,19 @@ export const Team = () => {
               // height={350}
               className="absolute w-full h-full rounded-lg"
             />
+            <div className="absolute bottom-9 w-full z-40">
+              <div className="flex flex-row gap-3 mx-auto w-[120px] h-[40px]  justify-center items-center rounded bg-[#AD8E6D] text-white">
+                <FaFacebookF />
+                <FaTwitter />
+                <FaLinkedinIn />
+                <FaPinterest />
+              </div>
+            </div>
           </div>
-          <div className='flex flex-col items-center justify-center gap-1 mt-3'>
-            <h4 className='text-white text-[20px] font-semibold text-center '>Sasha Welsh</h4>
+          <div className="flex flex-col items-center justify-center gap-1 mt-3">
+            <h4 className="text-white text-[20px] font-semibold text-center ">
+              Sasha Welsh
+            </h4>
             <h6>Senior Partner</h6>
           </div>
         </div>
@@ -44,9 +68,19 @@ export const Team = () => {
               // height={350}
               className="absolute w-full h-full rounded-lg"
             />
+            <div className="absolute bottom-9 w-full z-40">
+              <div className="flex flex-row gap-3 mx-auto w-[120px] h-[40px]  justify-center items-center rounded bg-[#AD8E6D] text-white">
+                <FaFacebookF />
+                <FaTwitter />
+                <FaLinkedinIn />
+                <FaPinterest />
+              </div>
+            </div>
           </div>
-          <div className='flex flex-col items-center justify-center gap-1 mt-3'>
-            <h4 className='text-white text-[20px] font-semibold text-center '>John Shepard</h4>
+          <div className="flex flex-col items-center justify-center gap-1 mt-3">
+            <h4 className="text-white text-[20px] font-semibold text-center ">
+              John Shepard
+            </h4>
             <h6>Associate</h6>
           </div>
         </div>
