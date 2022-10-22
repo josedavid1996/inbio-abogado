e3d7a4b5ace3065348db7ec7a4454c035d4ed8b9
jeanflores99 <ajiliof@gmail.com>
2022-10-22 11:03:27 -0500

    creando data para el component blogs

---
+++ src/mock/dataBlogs.ts
@@ -0,0 +1,50 @@
+/* eslint-disable comma-dangle */
+export interface IDataBlog {
+  img: string
+  tittle: string
+  description: string
+}
+const pathname = '/images/blogs'
+export const DataBolgs: IDataBlog[] = [
+  {
+    img: `${pathname}/1.jpg`,
+    tittle: 'The Lawyer European Awards shortlist',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+  {
+    img: `${pathname}/2.jpg`,
+
+    tittle: 'Six firms that are setting the trend',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+  {
+    img: `${pathname}/3.jpg`,
+
+    tittle: 'The Lawyer European Awards shortlist',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+  {
+    img: `${pathname}/4.jpg`,
+
+    tittle: 'Six firms that are setting the trend',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+  {
+    img: `${pathname}/5.jpg`,
+
+    tittle: 'The Lawyer European Awards shortlist',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+  {
+    img: `${pathname}/6.jpg`,
+
+    tittle: 'Six firms that are setting the trend',
+    description:
+      'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...',
+  },
+]
