358475e58da930e6e9cb4088d4af3e56488b1d87
jeanflores99 <ajiliof@gmail.com>
2022-10-29 11:57:32 -0500

    reparando seo

--- src/components/others/head.tsx
+++ src/components/others/head.tsx
@@ -3,8 +3,8 @@ import HeadNext from 'next/head'
 export const Head = () => {
   return (
     <HeadNext>
-      <title>Kyros - Personal Websote</title>
-      <meta property="og:title" content="My page title" key="title" />
+      <title>Kyros - Personal Website</title>
+      <meta property="og:title" content="Kyros - Personal Website" key="title" />
     </HeadNext>
   )
 }
