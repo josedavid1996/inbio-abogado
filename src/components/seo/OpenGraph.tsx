e4301b1c2f576a2c25cc561c09b8a088412dfb00
Jean Flores Alvarez <ajiliof@gmail.com>
2022-12-01 23:16:01 -0500

    probando seo con contextapi part 3

--- src/components/seo/OpenGraph.tsx
+++ src/components/seo/OpenGraph.tsx
@@ -1,36 +1,34 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-empty-pattern */
+import { useEffect } from 'react'
 import { IdataPageGeneral } from './data'
 import NextHead from 'next/head'
-export const OpenGraph = ({
-  description,
-  domain,
-  img,
-  keywords,
-  link,
-  tittlePage,
-  url,
-}: IdataPageGeneral) => (
-  <NextHead>
-    <title>{tittlePage}</title>
-    <meta name="description" content={description || ''} />
-    <meta name="keywords" content={keywords || ''} />
+export const OpenGraph = (data: IdataPageGeneral) => {
+  useEffect(() => {
+    console.log('cambando el meta')
+  }, [data])
+  return (
+    <NextHead>
+      <title>{data.tittlePage}</title>
+      <meta name="description" content={data.description || ''} />
+      <meta name="keywords" content={data.keywords || ''} />
 
-    <meta property="og:site_name" content={link} />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={tittlePage || ''} />
-    <meta property="og:description" content={description || ''} />
-    <meta property="og:url" content={url || ''} />
-    <meta property="og:image" content={img || ''} />
-    <meta property="og:locale" content="es_LA" />
+      <meta property="og:site_name" content={data.link} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={data.tittlePage || ''} />
+      <meta property="og:description" content={data.description || ''} />
+      <meta property="og:url" content={data.url || ''} />
+      <meta property="og:image" content={data.img || ''} />
+      <meta property="og:locale" content="es_LA" />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={tittlePage || ''} />
-    <meta name="twitter:description" content={description || ''} />
-    <meta property="twitter:domain" content={domain} />
-    <meta property="twitter:url" content={url || ''} />
-    <meta name="twitter:image" content={img || ''} />
-    <meta property="og:image:width" content="400" />
-    <meta property="og:image:height" content="300" />
-  </NextHead>
-)
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={data.tittlePage || ''} />
+      <meta name="twitter:description" content={data.description || ''} />
+      <meta property="twitter:domain" content={data.domain} />
+      <meta property="twitter:url" content={data.url || ''} />
+      <meta name="twitter:image" content={data.img || ''} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+    </NextHead>
+  )
+}
