f37ee973f70d4512d7890088e71d22bd138cabe5
Jodarepe <jodarepe@gmail.com>
2022-12-26 19:14:38 -0500

    feat:Se corrigio bug en el build

--- src/pages/blog/categoria/[slug].tsx
+++ src/pages/blog/categoria/[slug].tsx
@@ -11,7 +11,7 @@ import request from 'graphql-request'
 import { mode } from '@apollo/index'
 import {
   GET_ALL_BLOG_CATEGORIA_SLUG,
-  GET_CATEGORIA_BLOG_SLUG,
+  GET_CATEGORIA_BLOG_SLUG
 } from '@ssr/index'
 import { BlogDTO, CategoriaBlogDTO } from '@components/others/blog/interfaces'
 import { SetDataMeta } from '@Redux/Meta/mesaSlice'
@@ -25,10 +25,8 @@ interface IPropsSSP {
   GetCategoriaBlogSlug: CategoriaBlogDTO
 }
 const Index = ({ BlogsCategoriaSlug, GetCategoriaBlogSlug }: IPropsSSP) => {
-  const {
-    data: DataCategoryBlogs,
-    loading: LoadingCategorysBlogs,
-  } = useGetAllCategoriaBlogs()
+  const { data: DataCategoryBlogs, loading: LoadingCategorysBlogs } =
+    useGetAllCategoriaBlogs()
   // const {
   //   data: BlogsCategoriaSlug,
   //   loading: LoadingBlosCategoriaSlug,
@@ -48,8 +46,8 @@ const Index = ({ BlogsCategoriaSlug, GetCategoriaBlogSlug }: IPropsSSP) => {
             { description: 'Blog', url: '/blog' },
             { description: 'Categoría' },
             {
-              description: GetCategoriaBlogSlug.titulo || '',
-            },
+              description: GetCategoriaBlogSlug.titulo || ''
+            }
           ]}
         />
         <CategoriasBlog
@@ -72,51 +70,53 @@ const Index = ({ BlogsCategoriaSlug, GetCategoriaBlogSlug }: IPropsSSP) => {
 export default Index
 
 export const getServerSideProps = Wrapper.getServerSideProps(
-  (store: Store) => async ({ query }: GetServerSidePropsContext) => {
-    {
-      /** seleccionamos la uri atravez del env desarrollo */
-    }
-    const uri = mode[process.env.NEXT_PUBLIC_MODE || 'desarrollo']
-
-    {
-      /** llamamos la api, este metodo solo funciona para ssr */
-    }
-    const { GetAllBlogsCategoriaSlug } = await request(
-      uri,
-      GET_ALL_BLOG_CATEGORIA_SLUG,
-      { estado: 'Activado', numeroPagina: 20, pagina: 1, slug: query.slug },
-    )
-
-    {
-      /** usamos este query para actualizar el open graph */
-    }
-    const {
-      GetCategoriaBlogSlug,
-    }: { GetCategoriaBlogSlug: CategoriaBlogDTO } = await request(
-      uri,
-      GET_CATEGORIA_BLOG_SLUG,
+  (store: Store) =>
+    async ({ query }: GetServerSidePropsContext) => {
       {
-        slug: query.slug,
-      },
-    )
+        /** seleccionamos la uri atravez del env desarrollo */
+      }
+      const uri = mode[process.env.NEXT_PUBLIC_MODE || 'desarrollo']
 
-    store.dispatch(
-      SetDataMeta({
-        tittlePage: 'Kyros - ' + GetCategoriaBlogSlug.titulo,
-        link: GetCategoriaBlogSlug.titulo,
-        description: GetCategoriaBlogSlug.descripcion,
-        domain: process.env.NEXT_PUBLIC_DOMAIN + 'blog/categoria/' + query.slug,
-        imgPrincipal: GetCategoriaBlogSlug.imagenSecundaria.url,
-        imgSecundaria: GetCategoriaBlogSlug.imagenSecundaria.url,
-        keywords: GetCategoriaBlogSlug.keywords,
-        url: process.env.NEXT_PUBLIC_DOMAIN + 'blog/categoria/' + query.slug,
-      }),
-    )
-    return {
-      props: {
-        BlogsCategoriaSlug: GetAllBlogsCategoriaSlug.data,
-        GetCategoriaBlogSlug,
-      },
+      {
+        /** llamamos la api, este metodo solo funciona para ssr */
+      }
+      const { GetAllBlogsCategoriaSlug } = await request(
+        uri,
+        GET_ALL_BLOG_CATEGORIA_SLUG,
+        { estado: 'Activado', numeroPagina: 20, pagina: 1, slug: query.slug }
+      )
+
+      {
+        /** usamos este query para actualizar el open graph */
+      }
+      const {
+        GetCategoriaBlogSlug
+      }: { GetCategoriaBlogSlug: CategoriaBlogDTO } = await request(
+        uri,
+        GET_CATEGORIA_BLOG_SLUG,
+        {
+          slug: query.slug
+        }
+      )
+
+      store.dispatch(
+        SetDataMeta({
+          tittlePage: 'Kyros - ' + GetCategoriaBlogSlug.titulo,
+          link: GetCategoriaBlogSlug.titulo,
+          description: GetCategoriaBlogSlug.descripcionCorta,
+          domain:
+            process.env.NEXT_PUBLIC_DOMAIN + 'blog/categoria/' + query.slug,
+          imgPrincipal: GetCategoriaBlogSlug.imagenSecundaria.url,
+          imgSecundaria: GetCategoriaBlogSlug.imagenSecundaria.url,
+          keywords: GetCategoriaBlogSlug.keywords,
+          url: process.env.NEXT_PUBLIC_DOMAIN + 'blog/categoria/' + query.slug
+        })
+      )
+      return {
+        props: {
+          BlogsCategoriaSlug: GetAllBlogsCategoriaSlug.data,
+          GetCategoriaBlogSlug
+        }
+      }
     }
-  },
 )
