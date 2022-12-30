97769018502f1a6ef5cccfcef0354f3da1173324
jeanflores99 <ajiliof@gmail.com>
2022-12-30 10:00:47 -0500

    resolviendo errores del ppt

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,15 +1,7 @@
-/* eslint-disable n/handle-callback-err */
-/* eslint-disable no-undef */
-/* eslint-disable no-lone-blocks */
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
-/* eslint-disable comma-dangle */
-import { AllBlogs, WrapperButtonShares } from '@components/others/blog'
-import dynamic from 'next/dynamic'
+/* eslint-disable  */
+
+import { WrapperButtonShares } from '@components/others/blog'
 import { BlogDTO } from '@components/others/blog/interfaces'
-import { useGetBlogSlug } from '@Services'
-import { LayoutBlog } from '@components/layout/blog'
-import { useEffect, useState } from 'react'
 import NextImage from 'next/image'
 import request from 'graphql-request'
 import { GetServerSidePropsContext } from 'next'
@@ -17,20 +9,14 @@ import { Container } from '@components/others/home'
 import { IMG_404 } from '@mock/etc'
 import { URI } from '@Uri/index'
 import { GET_SLUG_BLOG } from '@ssr/index'
-import { CallSeoContext } from '@contexts/seo/SeoContext'
-import { OpenGraph } from '@components/seo/OpenGraph'
-import {
-  FaFacebook,
-  FaInstagram,
-  FaWhatsapp,
-  FaShare,
-  FaTwitter,
-} from 'react-icons/fa'
+import { FaFacebook, FaWhatsapp, FaTwitter } from 'react-icons/fa'
 import { useRouter, NextRouter } from 'next/router'
 import { Wrapper } from '@Redux/store'
 import { Store } from '@reduxjs/toolkit'
 import { SetDataMeta } from '@Redux/Meta/mesaSlice'
 import { BreadCrumbs } from '@components/shared'
+import moment from 'moment'
+import { CalculateTiempoPasado } from '@Util/CalculateTiempoPasado'
 interface IPropsSSP {
   slug: string
   data: BlogDTO
@@ -40,7 +26,37 @@ const Index = ({ data, slug }: IPropsSSP) => {
   const router = useRouter()
   const Page = router.pathname.split('/')[1]
   const MY_URL = process.env.NEXT_PUBLIC_DOMAIN + Page + '/' + slug
+  const fechaCreada = moment(data.created_at)
+  const fechaActual = moment(new Date())
 
+  const GroupBotom = () => (
+    <WrapperButtonShares
+      MetaData={{
+        text: data.descripcionCorta,
+        tittle: data.titulo,
+        url: MY_URL,
+      }}
+      RedesSociales={[
+        {
+          Icon: FaFacebook,
+          color: 'colorFb',
+          url: 'https://www.facebook.com/sharer/sharer.php?u=' + MY_URL,
+        },
+        {
+          Icon: FaWhatsapp,
+          url:
+            'https://web.whatsapp.com/send?text=' + data.titulo + ' ' + MY_URL,
+          color: 'colorWsp',
+        },
+        {
+          Icon: FaTwitter,
+          url:
+            'http://TWITTER.com/share?text=' + data.titulo + '&url=' + MY_URL,
+          color: 'colorTw',
+        },
+      ]}
+    />
+  )
   return (
     <>
       <div className="bg-[#171A1D] min-h-screen h-full">
@@ -53,8 +69,16 @@ const Index = ({ data, slug }: IPropsSSP) => {
               },
             ]}
           />
+          <div className="flex flex-row items-center justify-between mt-4">
+            <div className="text-gray-200 font-bold">
+              {data.CategoriaBlog.titulo}
+            </div>
+            <div className="flex flex-row gap-4">
+              <GroupBotom />
+            </div>
+          </div>
           <div className="flex flex-col gap-6 w-full py-4">
-            <div className="text-base md:text-2xl lg:text-3xl text-custon4 font-bold">
+            <div className="text-base md:text-2xl lg:text-3xl text-custon4 font-bold uppercase">
               {data?.titulo || ''}
             </div>
             <div className="relative aspect-video w-full">
@@ -71,43 +95,18 @@ const Index = ({ data, slug }: IPropsSSP) => {
               className="font-medium leading-8 text-md font-customText text-gray-200 "
               dangerouslySetInnerHTML={{ __html: data?.descripcionLarga! }}
             />
-            <div className="text-gray-100 w-max flex flex-col font-semibold relative after:absolute after:-bottom-1 after:w-full after:h-[1px] after:bg-custom1">
-              {data?.CategoriaBlog?.titulo || ''}
-            </div>
-            <div className="flex flex-row w-full gap-4 justify-end">
-              <WrapperButtonShares
-                MetaData={{
-                  text: data.descripcionCorta,
-                  tittle: data.titulo,
-                  url: MY_URL,
-                }}
-                RedesSociales={[
-                  {
-                    Icon: FaFacebook,
-                    color: 'colorFb',
-                    url:
-                      'https://www.facebook.com/sharer/sharer.php?u=' + MY_URL,
-                  },
-                  {
-                    Icon: FaWhatsapp,
-                    url:
-                      'https://web.whatsapp.com/send?text=' +
-                      data.titulo +
-                      ' ' +
-                      MY_URL,
-                    color: 'colorWsp',
-                  },
-                  {
-                    Icon: FaTwitter,
-                    url:
-                      'http://TWITTER.com/share?text=' +
-                      data.titulo +
-                      '&url=' +
-                      MY_URL,
-                    color: 'colorTw',
-                  },
-                ]}
-              />
+            <div className="flex flex-col  sm:flex-row gap-4 sm:items-center justify-start sm:justify-between">
+              <div className="flex flex-row gap-4 items-center">
+                <p className="font-bold text-red-200">Compatir:</p>
+                <div className="flex flex-row gap-2">
+                  <GroupBotom />
+                </div>
+              </div>
+              <p className="text-gray-200 font-bold flex  justify-end">
+                {CalculateTiempoPasado(
+                  fechaActual.diff(fechaCreada, 'minutes'),
+                )}
+              </p>
             </div>
           </div>
         </Container>
